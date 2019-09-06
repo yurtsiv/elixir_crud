68e943ef28c431f51794b752888a90f38d698003
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-09-06 12:47:14 +0200

    fix ArticlesTest

--- test/netguru_assignment/articles_test.exs
+++ test/netguru_assignment/articles_test.exs
@@ -2,19 +2,40 @@ defmodule NetguruAssignment.ArticlesTest do
   use NetguruAssignment.DataCase
 
   alias NetguruAssignment.Articles
+  alias NetguruAssignment.Authors
 
   describe "articles" do
     alias NetguruAssignment.Articles.Article
 
-    @valid_attrs %{body: "some body", description: "some description", published_date: ~N[2010-04-17 14:00:00], title: "some title"}
-    @update_attrs %{body: "some updated body", description: "some updated description", published_date: ~N[2011-05-18 15:01:01], title: "some updated title"}
+    @author_attrs %{age: 42, first_name: "some first_name", last_name: "some last_name"}
+    @valid_attrs %{
+      body: "some body",
+      description: "some description",
+      title: "some title"
+    }
+    @update_attrs %{
+      body: "some updated body",
+      description: "some updated description",
+      title: "some updated title"
+    }
     @invalid_attrs %{body: nil, description: nil, published_date: nil, title: nil}
 
+    def author_fixture() do
+      {:ok, author} =
+        %{}
+        |> Enum.into(@author_attrs)
+        |> Authors.create_author()
+
+      author
+    end
+
     def article_fixture(attrs \\ %{}) do
+      author = author_fixture()
+
       {:ok, article} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Articles.create_article()
+        |> Articles.create_article(author)
 
       article
     end
@@ -30,30 +51,21 @@ defmodule NetguruAssignment.ArticlesTest do
     end
 
     test "create_article/1 with valid data creates a article" do
-      assert {:ok, %Article{} = article} = Articles.create_article(@valid_attrs)
+      current_date = NaiveDateTime.utc_now()
+      assert {:ok, %Article{} = article} = Articles.create_article(@valid_attrs, author_fixture())
+
+      published_date = article.published_date
+      assert published_date.year == current_date.year
+      assert published_date.month == current_date.month
+      assert published_date.day == current_date.day
       assert article.body == "some body"
       assert article.description == "some description"
-      assert article.published_date == ~N[2010-04-17 14:00:00]
       assert article.title == "some title"
     end
 
     test "create_article/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Articles.create_article(@invalid_attrs)
-    end
-
-    test "update_article/2 with valid data updates the article" do
-      article = article_fixture()
-      assert {:ok, %Article{} = article} = Articles.update_article(article, @update_attrs)
-      assert article.body == "some updated body"
-      assert article.description == "some updated description"
-      assert article.published_date == ~N[2011-05-18 15:01:01]
-      assert article.title == "some updated title"
-    end
-
-    test "update_article/2 with invalid data returns error changeset" do
-      article = article_fixture()
-      assert {:error, %Ecto.Changeset{}} = Articles.update_article(article, @invalid_attrs)
-      assert article == Articles.get_article!(article.id)
+      assert {:error, %Ecto.Changeset{}} =
+               Articles.create_article(@invalid_attrs, author_fixture())
     end
 
     test "delete_article/1 deletes the article" do
